0c748aa286d1f581e360a2e26504aae8c2c056e2
fzagato <fzagato@gmail.com>
2016-01-10 20:32:41 -0600

    Initial Commit

---
+++ Phone Register/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Phone Register
+//
+//  Created by Francisco Javier Trujillo Lopez on 10/01/16.
+//  Copyright © 2016 FZagato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Phone Register/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Phone Register
+//
+//  Created by Francisco Javier Trujillo Lopez on 10/01/16.
+//  Copyright © 2016 FZagato. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
