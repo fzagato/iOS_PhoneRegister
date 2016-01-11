36a93affff03ad4eebadfd1ed96a0b5392896871
fzagato <fzagato@gmail.com>
2016-01-10 23:27:56 -0600

    initial configuration

--- Phone Register/Register.m
+++ Phone Register/Register.m
@@ -6,13 +6,13 @@
 //  Copyright © 2016 FZagato. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "Register.h"
 
-@interface ViewController ()
+@interface Register ()
 
 @end
 
-@implementation ViewController
+@implementation Register
 
 - (void)viewDidLoad {
     [super viewDidLoad];
